d3e633e75b3960db7b97be7c6a4f288589013d83
kumikomi21 <blueskyjob11@gmail.com>
2023-03-10 17:11:20 +0900

    サーボ訂正完了。

--- servo/ServoMotorDoor.cpp
+++ servo/ServoMotorDoor.cpp
@@ -6,19 +6,18 @@ include "servo.h"
 
 //Brainがタイムチェック関数で時刻確認して起床時刻になったとき
 
-open() {
-  myservo.write(servoClose); //サーボを動かす角度を指定する
-};
+void ServoMotorDoor::open(){
+  myservo.write(servoOpen); //サーボを動かす角度を指定する
+}
 
 //Brainがタイムチェック関数で時刻確認して就寝時刻になったとき
 
-close(){
-    myservo.write(servoOpen); //サーボを動かす角度を指定する
-
-};
+void ServoMotorDoor::close(){
+    myservo.write(servoClose); //サーボを動かす角度を指定する
+}
 
 //Brainが緊急ボタンを感知したとき
 
-emergency() {
+void ServoMotorDoor::emergency() {
   myservo.write(servoOpen); //サーボを動かす角度を指定する
-};
+}
--- servo/ServoMotorDoor.h
+++ servo/ServoMotorDoor.h
@@ -3,17 +3,19 @@
 class ServoMotorDoor
 {
   private:
+
    int servoOpen = 90;
    int servoClose = 0;
    void emergency();
    
   Servo myservo;  //スケッチ内で使用するサーボの名前を宣言
+                  //has aの関係　ServoMotorDoor　has a Servo
 
    public:
    void open();
    void close();
    
-}   
+};   
 
    
 //myservo.attach(7, 500, 2400); //スケッチ内で使用するサーボの名前を宣言
