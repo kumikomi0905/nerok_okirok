d16e0e853f5937b43b22839738b7455c59f78aec
kumikomi05 <poly.tamaki@gmail.com>
2023-03-13 15:18:29 +0900

    Displayクラスに起床・就寝の時間設定ボタンの関数を追加

--- Brain/CurrentTime.cpp
+++ Brain/CurrentTime.cpp
@@ -1,16 +1,1 @@
-#include<Arduino.h>
-#include"Brain.h"
-#include"CurrentTime.h"
 
-//後で記述
-int time_Check(){
-  
-}
-
-void setrealTime(){
-  
-}
-
-int send_CurrentTime(){
-  
-}
--- Brain/CurrentTime.h
+++ Brain/CurrentTime.h
@@ -1,15 +1,1 @@
-#pragma once
 
-class CurrentTime{
-  private:
-    int espNumber;            //espのピン番号
-    int realTime;             //RTCのピン番号
-
-
-  public:
-    int time_Check();                         //時間の比例
-    void setrealTime();                       //RTCのセット
-    int send_CurrentTime();                   //RTCの設定を送る
-}
-
-#endif
