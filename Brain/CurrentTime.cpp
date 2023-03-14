6ab15715d46594e2d3deaba2f140d0e1356de458
kumikomi14 <kinjyoyuya@gmail.com>
2023-03-14 14:12:15 +0900

    CurrentTime関数追加

--- Brain/CurrentTime.cpp
+++ Brain/CurrentTime.cpp
@@ -1,1 +1,20 @@
+#include<Arduino.h>
+#include"Brain.h"
+#include"CurrentTime_h"
 
+int time_Check(){
+if(){
+
+}
+  
+}
+
+void setrealTime(){
+
+  
+}
+
+int send_CurrentTime(){
+
+  
+}
--- Brain/CurrentTime.h
+++ Brain/CurrentTime.h
@@ -1,1 +1,19 @@
+#pragma once
 
+class CurrentTime {
+
+  private:
+    int espNumber;            //espのピン番号
+    int realTime;             //RTCのピン番号
+
+
+  public:
+    int Time_Check();
+    void setrealTime();
+    int send_CurrentTime();
+
+}
+
+
+
+#endif
