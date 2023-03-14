53dab00e1d50c1c8dbf3b482bf1589e8bc49fbc5
kumikomi21 <blueskyjob11@gmail.com>
2023-03-14 11:41:50 +0900

    LedLightの大枠。

--- servo/LedLight.cpp
+++ servo/LedLight.cpp
@@ -1,28 +1,87 @@
 #include <Arduino.h>
 #include"LedLight.h"
 
-//起床ライト機能
+//コンストラクタ
+LedLight(){
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+//いきなり光るということが無い様、念のためlowに初期化しておく
+ digitalWrite(REDPIN, LOW);
+ digitalWrite(BLUEPIN, LOW);
+ digitalWrite(GREENPIN, LOW);
+}
+
+//起床ライト機能(起床ライトは朝日のイメージで白色点灯)
 //Brainが起床時刻になったと感知したら日の出ライトを呼び出し
 
-ledGetup(){
-  //ライト点灯の関数を調べる
-  
+void LedLight::ledGetup(){
+  //ライト点灯(3色点灯で白色の予定)
+  digitalWrite(GREENPIN,HIGH);
+  digitalWrite(REDPIN,HIGH);
+  digitalWrite(BLUEPIN,HIGH);
+ 
 }
 
 //Brainがサブボタンで起床を確認したら日の出ライト消灯を呼び出し
 
-ledOff(){
-  //ライト消灯の関数を調べる
+void LedLight::ledOff(){
+  //ライト消灯
+  digitalWrite(GREENPIN,LOW);
+  digitalWrite(REDPIN,LOW);
+  digitalWrite(BLUEPIN,LOW);
 }
 
 
-//就寝ライト機能
+//就寝ライト機能(就寝ライトは温かみのある落ち着いた暖色系の予定)
 //Brainが現在時刻取得より就寝10分前になったらライト10秒間点滅
 //その後点灯
 
-ledSleepy(){
-  //Led10秒点滅関数を呼び出す
+void LedLight::ledSleepy(){
+  //Led10秒点滅
+  void loop() {
+   for (int i=0; i <= 10; i++){
+      digitalWrite(GREENPIN,HIGH);
+      digitalWrite(REDPIN,HIGH);
+      delay(500);
+      digitalWrite(GREENPIN,LOW);
+      digitalWrite(REDPIN,LOW);
+      delay(500);
+      }
+  //その後Ledライトが10分の間に5段階で2分ごとに明るさレベルを落としていく関数呼び出す
+  //そして完全消灯になる
 
-  //その後Ledライトが5段階で2分ごとに明るさレベルを落としていく関数呼び出す
-  //そして10分後完全消灯になる
+  //明るさ：1番明るい
+      analogWrite( GREENPIN, max_bright );
+      analogWrite( REDPIN, max_bright );
+      delay(120000);  //2分待つ
+  
+  //明るさ：明るい
+      analogWrite( GREENPIN, bright );
+      analogWrite( REDPIN, bright );
+      delay(120000);  //2分待つ
+
+  //明るさ：真ん中の明るさ
+      analogWrite( GREENPIN, middle_bright );
+      analogWrite( REDPIN, middle_bright );
+      delay(120000);  //2分待つ
+
+  //明るさ：暗い
+      analogWrite( GREENPIN, dark );
+      analogWrite( REDPIN, dark );
+      delay(120000);  //2分待つ
+
+  //明るさ：1番暗い
+      analogWrite( GREENPIN, max_dark );
+      analogWrite( REDPIN, max_dark );
+      delay(120000);  //2分待つ
+
+  //消灯
+      digitalWrite(GREENPIN,LOW);
+      digitalWrite(REDPIN,LOW);
+
+
+   
+  
+  
 }
--- servo/LedLight.h
+++ servo/LedLight.h
@@ -1,5 +1,27 @@
 #pragma once
 
+//2021.4月A班のソースコード参考
+
+/*
+ * 白を含めた7色の光が点灯する
+ * デバッグ用
+ */
+#define GREENPIN 9
+#define REDPIN 10
+#define BLUEPIN 11
+#define DELAY_TIME 3000
+
+typedef enum{
+ RED,
+ GREEN,
+ BLUE,
+ YELLOW,
+ SKYBLUE,
+ PURPLE,
+ WHITE
+}LED_COLOR;
+
+
 class LedLight {
 
   private:
@@ -7,14 +29,21 @@ class LedLight {
     int Greenpwm;
     int Bluepwm;
     int Get_up;
-    int Every_Bed = 120   //2分おきに
-    int Before_Bed = 600  //10分前
-    int Blink_Bed = 10    //10秒前
+    int Every_Bed = 120;   //2分おきに
+    int Before_Bed = 600;  //10分前
+    int Blink_Bed = 10;    //10秒前
     int ledColor;
+    int max_bright = 255;       //1番明るい
+    int bright = 191;           //明るい
+    int middle_bright = 127;    //真ん中の明るさ
+    int dark = 87;             //暗い
+    int max_dark = 47;         //1番暗い
+
 
 
   public:
     void ledSleepy();
     void ledGetup();
     void ledOff();
-}
+    LedLight(); //コンストラクタ
+};
